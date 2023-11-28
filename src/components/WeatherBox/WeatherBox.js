b89841f47cdc144575412a592b6b513a50a27ff5
Piotr Węgłowski <weglowskipiotr@gmail.com>
2023-11-28 21:30:29 +0100

    Add props to WeatherSummary component

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,28 +2,35 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback } from 'react';
+import { useState } from 'react';
 
 const WeatherBox = () => {
+  const [weatherData, setWeatherData] = useState('');
+
   const handleCityChange = useCallback((city) => {
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ab78bceb2aaeb41c58454548196c5e9a&units=metric`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        const weatherData = {
+        setWeatherData({
           city: data.name,
           temp: data.main.temp,
           icon: data.weather[0].icon,
           description: data.weather[0].main,
-        };
+        });
       });
   }, []);
 
   return (
     <section>
       <PickCity handleCityChange={handleCityChange} />
-      <WeatherSummary />
+      <WeatherSummary
+        city={weatherData.city}
+        temp={weatherData.temp}
+        icon={weatherData.icon}
+        description={weatherData.description}
+      />
       <Loader />
     </section>
   );
